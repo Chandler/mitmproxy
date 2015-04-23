4c04b80decff2dd38beafc4cb8f19e91e8152468
Chandler Abraham <cabraham@twitter.com>
2015-04-22 20:53:29 -0700

    saving

--- libmproxy/protocol/ws.py
+++ libmproxy/protocol/ws.py
@@ -1,9 +1,54 @@
 from __future__ import absolute_import
 import select
 import socket
-from .primitives import ProtocolHandler
+from .primitives import ProtocolHandler, Flow
 from netlib.utils import cleanBin
 from netlib.tcp import NetLibError
+from . import http
+from netlib.websockets import websockets as ws
+
+class WebSocketsConnectionFlow(Flow):
+    """
+        A type of flow which enacapsulates a single WebSockets connection
+        and optionally contains a list of any frames that have been passed over this connection
+    """
+    def __init__(self, client_conn, server_conn, live = None):
+        super(WebSocketsConnectionFlow, self).__init__("ws", client_conn, server_conn, live)
+        
+        self.clientHandshake = None
+        """@type: http.HTTPRequest"""
+        
+        self.serverHandshake = None        
+        """@type: http.HTTPResponse"""
+      
+        self.frames = []
+        """@type: [WebSocketsFrame]"""
+
+        _stateobject_attributes = Flow._stateobject_attributes.copy()
+        _stateobject_attributes.update(
+            clientHandshake=http.HTTPRequest,
+            serverHandshake=http.HTTPResponse
+        )
+
+
+class WebSocketsFrameFlow(Flow):
+    """
+        A type of flow which enacapsulates a single WebSockets frame
+        optionally contains a reference to WebSocketsConnectionFlow corresponding
+        to the connection this frame was sent over. 
+    """
+    def __init__(self, frame, ws_connection_flow = None):
+        super(WebSocketsFrameFlow, self).__init__(
+            "ws",
+            ws_connection_flow.client_conn,
+            ws_connection_flow.server_conn,
+            ws_connection_flow.live
+        )
+        self.parent_flow = ws_connection_flow
+        """@type: WebSocketsConnectionFlow"""
+
+        self.frame = frame
+        """@type: WebSocketsFrame"""
 
 class WebSocketsHandler(ProtocolHandler):
     chunk_size = 4096
@@ -19,6 +64,8 @@ class WebSocketsHandler(ProtocolHandler):
         buf = memoryview(bytearray(self.chunk_size))
 
         conns = [self.c.client_conn.rfile, self.c.server_conn.rfile]
+
+        conn_flow = WebSocketsConnectionFlow(self.c.client_conn, self.c.server_conn, self.live)
 
         try:
             while True:
@@ -47,12 +94,27 @@ class WebSocketsHandler(ProtocolHandler):
                             return
                         continue
  
-                    mitmdump_output = "Websockets frame"
+                    mitmdump_output = "WebSockets frame"
               
                     self.c.log(mitmdump_output, "info")
 
-                    dst.connection.send(buf[:size])
+                    # take the bytes and construct a websockets frame and frame_flow
+                    frame_bytes = buf[:size].tobytes()
+                    # try:
+                    frame       = ws.WebSocketsFrame.from_bytes(frame_bytes)
+                    frame_flow  = WebSocketsFrameFlow(frame, conn_flow)
+                    self.c.log(frame, "info")
 
+                    # send flows to flowmaster
+                    self.c.channel.tell("websockets_connection", conn_flow)
+                    self.c.channel.tell("websockets_frame", frame_flow)
+                    # except:
+                    #     return
+
+                    # pass bytes onto their destination regardless of if we were able to parse them
+                    dst.connection.send(buf[:size])
         except (socket.error, NetLibError) as e:
             self.c.log("TCP connection closed unexpectedly.", "debug")
             return
+
+
